b5560acabcfea864e41b78af372a178ac8ef931d
Austin O <austino@iAir.local>
2024-11-30 17:17:35 -0500

    Added console app and finalized library

---
+++ StringExtensionsConsole/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using StringExtensionsNamespace;
+
+namespace StringExtensionsConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine("Enter a string:");
+        string input = Console.ReadLine();
+
+        bool result = input.StartsWithUpper();
+        Console.WriteLine(result
+            ? "The string starts with an uppercase letter."
+            : "The string does not start with an uppercase letter.");
+        }
+    }
+}
